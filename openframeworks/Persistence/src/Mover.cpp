bdc0711e7fe0a2d70ad44b256bde149f4d7298dd
Mike Chambers <mikechambers@gmail.com>
2016-04-18 09:58:02 -0700

    adding screenshot directory

---
+++ openframeworks/Persistence/src/Mover.cpp
@@ -0,0 +1,9 @@
+//
+//  Mover.cpp
+//  Persistence
+//
+//  Created by Mike Chambers on 4/17/16.
+//
+//
+
+#include "Mover.hpp"
---
+++ openframeworks/Persistence/src/Mover.hpp
@@ -0,0 +1,14 @@
+//
+//  Mover.hpp
+//  Persistence
+//
+//  Created by Mike Chambers on 4/17/16.
+//
+//
+
+#ifndef Mover_hpp
+#define Mover_hpp
+
+#include <stdio.h>
+
+#endif /* Mover_hpp */
