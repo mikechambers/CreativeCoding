dca21d0213779a58002e9d8d4f7e2f08b7899d92
Mike Chambers <mikechambers@gmail.com>
2016-05-04 09:06:25 -0700

    updating template

--- openframeworks/lib/of_v0.9.3_osx_release/scripts/templates/osx/src/ofApp.cpp
+++ openframeworks/lib/of_v0.9.3_osx_release/scripts/templates/osx/src/ofApp.cpp
@@ -1,23 +1,55 @@
 #include "ofApp.h"
 
+#include "ofxSyphonClient.h"
+#include "MeshUtils.h"
+
+
+MeshUtils utils;
+ofxSyphonServer syphon;
+
+const string APP_NAME = "APP_NAME";
+
+bool paused = false;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
+    utils.enableScreenshot(APP_NAME);
+    syphon.setName("APP_NAME");
+    
+    ofSetBackgroundAuto(true);
+    ofSetBackgroundColor(ofColor::white);
+    
+    init();
+}
 
+void ofApp::init() {
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    if(paused) {
+        return;
+    }
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    if(paused) {
+        syphon.publishScreen();
+        return;
+    }
+    
+    
+    syphon.publishScreen();
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    if(key == ' ') {
+        paused = !paused;
+    }
 }
 
 //--------------------------------------------------------------
--- openframeworks/lib/of_v0.9.3_osx_release/scripts/templates/osx/src/ofApp.h
+++ openframeworks/lib/of_v0.9.3_osx_release/scripts/templates/osx/src/ofApp.h
@@ -20,5 +20,5 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+        void init();
 };
