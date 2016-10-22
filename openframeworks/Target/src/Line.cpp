990d5b975ff7eefd125696b5c6ed007b7c05d5d9
Mike Chambers <mikechambers@gmail.com>
2016-10-21 21:19:36 -0700

    updated default templates

---
+++ openframeworks/Target/src/Line.cpp
@@ -0,0 +1,14 @@
+//
+//  Line.cpp
+//  Target
+//
+//  Created by Mike Chambers on 10/21/16.
+//
+//
+
+#include "Line.h"
+
+Line::Line() {
+    p1 = ofVec3f(0.0, 0.0, 0.0);
+    p2 = ofVec3f(0.0, 0.0, 0.0);
+}
