0d43f7ed8f5f8c81fe25bb2ee3916bcf1199297b
Mike Chambers <mikechambers@gmail.com>
2018-04-30 10:38:41 -0700

    latest tweaks.

--- openframeworks/EasingGradients/src/ofApp.cpp
+++ openframeworks/EasingGradients/src/ofApp.cpp
@@ -18,7 +18,7 @@
 
 string APP_NAME = ofFilePath::getFileName(ofFilePath::getCurrentExePath());
 
-bool const CLAMP_COLOR_VALUES = false;
+bool const CLAMP_COLOR_VALUES = false ;
 
 bool paused = false;
 
@@ -66,11 +66,11 @@ void ofApp::init() {
     //c2 = ofColor::black;
     
     //0x00C9FF, 0x92FE9D
-    //0x0052D4, 0x0052D4
+    //0xef32d9, 0x89fffd
     //0x9CECFB, 0x0052D4
     
-    c1 = ofColor::fromHex(0x00C9FF);//0x9CECFB
-    c2 = ofColor::fromHex(0x92FE9D);//0x0052D4
+    c1 = ofColor::fromHex(0xef32d9);//0x9CECFB
+    c2 = ofColor::fromHex(0x89fffd);//0x0052D4
     
     cout << "c1 : " << mColorToString(c1) << endl;
     cout << "c2 : " << mColorToString(c2) << endl;
@@ -93,7 +93,7 @@ void ofApp::update(){
     //c change between values
     //d length / duration
     
-    ofxeasing::function easing = ofxeasing::back::easeInOut;
+    ofxeasing::function easing = ofxeasing::circ::easeInOut;
     
     //
     float t = xPos;
