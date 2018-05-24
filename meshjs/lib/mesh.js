a76ac46c3173753bbce6113e052486ce026fee97
Mike Chambers <mikechambers@gmail.com>
2018-05-24 15:06:08 -0700

    Moved internal implimentation to class
    Move internal implimentation in module to a class instance. No API impacts, but a bit cleaner and should make it easier to expand in future.

--- meshjs/lib/mesh.js
+++ meshjs/lib/mesh.js
@@ -2,131 +2,144 @@ import Canvas from "./canvas.js"
 import {createFileName} from "./datautils.js"
 import Rectangle from "./rectangle.js"
 
-//may eventually allow these to be set via config object
-let _pauseKey = " ";
-let _downloadPngKey = "p";
-let _downloadVideoKey = "v";
-let _initKey = "i";
+class MeshJS {
+	constructor() {
+		//may eventually allow these to be set via config object
+		this._pauseKey = " ";
+		this._downloadPngKey = "p";
+		this._downloadVideoKey = "v";
+		this._initKey = "i";
 
-let _config;
-let _init;
-let _draw;
+		this._config;
+		this._init;
+		this._draw;
 
-let fpsInterval;
-let lastFrameTime;
-let startTime;
-let _paused = false;
+		this._fpsInterval;
+		this._lastFrameTime;
+		this._startTime;
+		this._paused = false;
 
-let _canvas;
-let _frameCount = 0;
-
-export function init(config, initCallback, drawCallback){
-
-	_config = config;
-	_init = initCallback;
-	_draw = drawCallback;
-
-	document.body.style.background = _config.BACKGROUND_COLOR;
-
-	window.addEventListener("keyup", onKeyUp);
-
-	if(_config.ANIMATE) {
-		fpsInterval = 1000 / config.FPS;
-		lastFrameTime = Date.now();
-		startTime = lastFrameTime;
-		window.requestAnimationFrame(onAnimationFrame);
+		this._canvas;
+		this._frameCount = 0;
 	}
 
-	_canvas = new Canvas(
-		_config.PARENT_ID,
-		_config.RENDER_WIDTH,
-		_config.RENDER_HEIGHT,
-		_config.CANVAS_BACKGROUND_COLOR);
+	init(config, initCallback, drawCallback){
 
-	if(_config.MAX_DISPLAY_WIDTH != _config.RENDER_WIDTH ||
-		_config.MAX_DISPLAY_HEIGHT != _config.RENDER_HEIGHT) {
+		this._config = config;
+		this._init = initCallback;
+		this._draw = drawCallback;
 
-		let maxW = _config.MAX_DISPLAY_WIDTH;
-		let maxH = _config.MAX_DISPLAY_HEIGHT;
+		document.body.style.background = this._config.BACKGROUND_COLOR;
 
-		let canvasW = _config.RENDER_WIDTH;
-		let canvasH = _config.RENDER_HEIGHT;
+		//using arrow function so we can capture and pass correct this scope
+		//window.addEventListener("keyup", (event) => { this.onKeyUp(event); });
+		window.addEventListener("keyup", this.onKeyUp.bind(this));
 
-		if (canvasH > maxH || canvasW > maxW) {
-			let ratio = canvasH / canvasW;
-
-			if (canvasW >= maxW && ratio <= 1) {
-				canvasW = maxW;
-				canvasH = canvasW * ratio;
-			} else if (canvasH >= maxH) {
-				canvasH = maxH;
-				canvasW = canvasH / ratio;
-			}
+		if(this._config.ANIMATE) {
+			this._fpsInterval = 1000 / this._config.FPS;
+			this._lastFrameTime = Date.now();
+			this._startTime = this._lastFrameTime;
+			window.requestAnimationFrame(this.onAnimationFrame.bind(this));
 		}
 
-		_canvas.canvas.style.height = canvasH;
-		_canvas.canvas.style.width = canvasW;
-	}
+		this._canvas = new Canvas(
+			this._config.PARENT_ID,
+			this._config.RENDER_WIDTH,
+			this._config.RENDER_HEIGHT,
+			this._config.CANVAS_BACKGROUND_COLOR);
 
-	if(_config.RECORD_VIDEO) {
-		_canvas.startRecord();
-	}
+		if(this._config.MAX_DISPLAY_WIDTH != this._config.RENDER_WIDTH ||
+			this._config.MAX_DISPLAY_HEIGHT != this._config.RENDER_HEIGHT) {
 
-	_init(_canvas);
+			let maxW = this._config.MAX_DISPLAY_WIDTH;
+			let maxH = this._config.MAX_DISPLAY_HEIGHT;
 
-	if(!_config.ANIMATE) {
-		draw();
-	}
-}
+			let canvasW = this._config.RENDER_WIDTH;
+			let canvasH = this._config.RENDER_HEIGHT;
 
-function draw() {
-	_frameCount++;
-	_draw(_canvas, _frameCount);
-}
+			if (canvasH > maxH || canvasW > maxW) {
+				let ratio = canvasH / canvasW;
 
-export function setPaused(paused) {
-	_paused = paused;
-	console.log(_paused?"paused":"running");
-}
-
-const onAnimationFrame = function() {
-	let now = Date.now();
-	let elapsed = now - lastFrameTime;
-
-	if(elapsed > fpsInterval) {
-		lastFrameTime = now - (elapsed % fpsInterval);
-
-		if(_draw && !_paused) {
-			if(_config.CLEAR_CANVAS) {
-				_canvas.clear();
+				if (canvasW >= maxW && ratio <= 1) {
+					canvasW = maxW;
+					canvasH = canvasW * ratio;
+				} else if (canvasH >= maxH) {
+					canvasH = maxH;
+					canvasW = canvasH / ratio;
+				}
 			}
-			draw();
+
+			this._canvas.canvas.style.height = canvasH;
+			this._canvas.canvas.style.width = canvasW;
+		}
+
+		if(this._config.RECORD_VIDEO) {
+			this._canvas.startRecord();
+		}
+
+		//this may get called in a weird scope now
+		this._init(this._canvas);
+
+		if(!this._config.ANIMATE) {
+			this.draw();
 		}
 	}
 
-	window.requestAnimationFrame(onAnimationFrame);
-}
+	draw() {
+		this._frameCount++;
+		this._draw(this._canvas, this._frameCount);
+	}
 
-export function downloadPng() {
-	let n = createFileName(_config.APP_NAME, "png");
-	_canvas.downloadPNG(n);
-}
+	setPaused(paused) {
+		this._paused = paused;
+		console.log(this._paused?"paused":"running");
+	}
 
-export function downloadVideo() {
-	let n = createFileName(_config.APP_NAME, "webm");
-	_canvas.downloadVideo(n);
-}
+	onAnimationFrame() {
+		let now = Date.now();
+		let elapsed = now - this._lastFrameTime;
 
-const onKeyUp = function(event){
-	const key = event.key;
+		if(elapsed > this._fpsInterval) {
+			this._lastFrameTime = now - (elapsed % this._fpsInterval);
 
-	if(key === _downloadPngKey) {
-		downloadPng();
-	} else if(key === _downloadVideoKey) {
-		downloadVideo();
-	} else if(key === _pauseKey) {
-		setPaused(!_paused);
-	} else if (key === _initKey) {
-		_init(_canvas);
+			if(this._draw && !this._paused) {
+				if(this._config.CLEAR_CANVAS) {
+					this._canvas.clear();
+				}
+				this.draw();
+			}
+		}
+
+		window.requestAnimationFrame(this.onAnimationFrame.bind(this));
+	}
+
+	downloadPng() {
+		let n = createFileName(this._config.APP_NAME, "png");
+		this._canvas.downloadPNG(n);
+	}
+
+	downloadVideo() {
+		let n = createFileName(this._config.APP_NAME, "webm");
+		this._canvas.downloadVideo(n);
+	}
+
+	onKeyUp(event){
+
+		const key = event.key;
+
+		if(key === this._downloadPngKey) {
+			this.downloadPng();
+		} else if(key === this._downloadVideoKey) {
+			this.downloadVideo();
+		} else if(key === this._pauseKey) {
+			this.setPaused(!this._paused);
+		} else if (key === this._initKey) {
+			this._init(this._canvas);
+		}
 	}
 }
+
+//todo: may want to make this static, so if we call from other modules we get
+//same instance
+let mesh;
+export default mesh = new MeshJS();
