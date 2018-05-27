b3b6ca8e6d088cd5bb87cf6f49f499e9ebbf08b5
Mike Chambers <mikechambers@gmail.com>
2018-05-27 00:14:19 -0700

    Clean up formating, added getRandom method

--- meshjs/lib/color.js
+++ meshjs/lib/color.js
@@ -1,3 +1,4 @@
+import { randomInt } from "./math.js";
 
 /**
  * Represents RGBA colors.
@@ -5,7 +6,6 @@
  * let color = new new Color(255, 255, 0, 1.0);
  */
 export default class Color {
-
 	/**
 	 * Constructor.
 	 * @param {number} r Red value of color, between 0 - 255.
@@ -14,8 +14,7 @@ export default class Color {
 	 * @param {float} a Alpha value of color between 0.0 and 1.0.
 	 */
 	constructor(r = 0, g = 0, b = 0, a = 1.0) {
-
-		if(arguments.length === 1) {
+		if (arguments.length === 1) {
 			g = r;
 			b = r;
 		}
@@ -67,17 +66,18 @@ export default class Color {
 	}
 
 	/**
-	* Whether the two colors represent the same RGB color.
-	* @param {Color} color - Color instance to compare for equality against this instance.
-	* @param {Boolean} ignoreAlpha - If true, alpha value will be ignored when comparing
-	* color equality. Otherwise alpha must also be equal.
-	*/
+	 * Whether the two colors represent the same RGB color.
+	 * @param {Color} color - Color instance to compare for equality against this instance.
+	 * @param {Boolean} ignoreAlpha - If true, alpha value will be ignored when comparing
+	 * color equality. Otherwise alpha must also be equal.
+	 */
 	isEqual(color, ignoreAlpha = true) {
-		let isEqual = (this._r === color.red &&
-				this._g === color.green &&
-				this._b === color.blue);
+		let isEqual =
+			this._r === color.red &&
+			this._g === color.green &&
+			this._b === color.blue;
 
-		if(!ignoreAlpha) {
+		if (!ignoreAlpha) {
 			isEqual = isEqual && this._a === color.alpha;
 		}
 
@@ -90,18 +90,23 @@ export default class Color {
 
 	//todo: include alpha output support?
 	toHex() {
-		return "#" + numberToHexString(this._r) + numberToHexString(this._g) + numberToHexString(this._b);
+		return (
+			"#" +
+			numberToHexString(this._r) +
+			numberToHexString(this._g) +
+			numberToHexString(this._b)
+		);
 	}
 
 	clone() {
 		return new Color(this._r, this._g, this._b, this._a);
 	}
 
-	static get BLACK(){
+	static get BLACK() {
 		return new Color(0).clone();
 	}
 
-	static get WHITE(){
+	static get WHITE() {
 		return new Color(255).clone();
 	}
 
@@ -112,38 +117,41 @@ export default class Color {
 	static getRGBA(r, g, b, a) {
 		return `rgba(${r}, ${g}, ${b}, ${a})`;
 	}
+
+	static getRandom(alpha = 1.0) {
+		return new Color(randomInt(255), randomInt(255), randomInt(255), alpha);
+	}
 }
 
 function numberToHexString(num) {
 	let hex = num.toString(16);
 
-	if(hex.length == 1) {
+	if (hex.length == 1) {
 		hex = `0${hex}`;
 	}
 
-	return hex
+	return hex;
 }
 
 //https://stackoverflow.com/a/5624139/10232
 //todo: need to add support for RGBA
 function hexToRgb(hex) {
-
 	//todo: need to check if a number is being passed in:
 	//0xFFFFFF
 
-	if(!hex.startsWith("#")) {
+	if (!hex.startsWith("#")) {
 		hex = "#" + hex;
 	}
 
 	//console.log(hex);
 
-    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-        return r + r + g + g + b + b;
-    });
+	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+	hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+		return r + r + g + g + b + b;
+	});
 
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 
 	if (!result) {
 		return undefined;
