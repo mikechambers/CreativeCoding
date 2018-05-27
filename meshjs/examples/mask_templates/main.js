8a838262241d5af00e1562b9c38d3d42f2fa84e8
Mike Chambers <mikechambers@gmail.com>
2018-05-27 12:05:34 -0700

    Adding npm, mocha, babel, chai support and tools
    Config files for npm, mocha, babel and chai tools for unit testing.

---
+++ meshjs/examples/mask_templates/main.js
@@ -0,0 +1,82 @@
+/**
+	Mike Chambers
+	https://github.com/mikechambers
+	http://www.mikechambers.com
+
+	Released under an MIT License
+	Copyright Mike Chambers 2018
+**/
+
+import mesh from "../../lib/mesh.js";
+import Rectangle from "../../lib/rectangle.js";
+import { loadPixelDataFromPathWithBounds } from "../../lib/pixeldata.js";
+
+/************ CONFIG **************/
+
+const config = {
+  /**** required for mesh lib ******/
+
+  //name of container that generated canvas will be created in
+  PARENT_ID: "canvas_container",
+
+  //app name, used for saving files
+  APP_NAME: window.location.pathname.replace(/\//gi, ""),
+
+  //Dimensions that canvas will be rendered at
+  RENDER_HEIGHT: 640,
+  RENDER_WIDTH: 1080,
+
+  //Max dimension canvas will be display at on page
+  //note, exact dimension will depend on RENDER_HEIGHT / width and
+  //ratio to these properties.
+  //Canvas display will be scaled to maintain aspect ratio
+  MAX_DISPLAY_HEIGHT: 640,
+  MAX_DISPLAY_WIDTH: 640,
+
+  //background color of html page
+  BACKGROUND_COLOR: "#DDDDDD",
+
+  //background color for display and offscreen canvas
+  CANVAS_BACKGROUND_COLOR: "#FF0000",
+
+  //whether a single frame is rendered, or draw is called based on FPS setting
+  ANIMATE: false,
+  FPS: 60,
+
+  //Where video of canvas is recorded
+  RECORD_VIDEO: false,
+
+  //whether canvas should be cleared prior to each call to draw
+  CLEAR_CANVAS: false,
+
+  /*********** APP Specific Settings ************/
+  TEMPLATE: "mask.gif"
+};
+
+/************** GLOBAL VARIABLES ************/
+
+let ctx;
+let bounds;
+let _pd;
+
+/*************** CODE ******************/
+
+const init = function(canvas) {
+  ctx = canvas.context;
+  bounds = canvas.bounds;
+};
+
+const draw = function(canvas, frameCount) {
+  ctx.putImageData(_pd.imageData, 0, 0);
+};
+
+window.onload = function() {
+  loadPixelDataFromPathWithBounds(
+    config.TEMPLATE,
+    function(pd, img) {
+      _pd = pd;
+      mesh.init(config, init, draw);
+    },
+    new Rectangle(0, 0, config.RENDER_WIDTH, config.RENDER_HEIGHT)
+  );
+};
--- meshjs/prettier.config.js
+++ meshjs/prettier.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-	parser: "babylon",
-	printWidth: 80,
-	tabWidth: 4,
-	useTabs: true,
-	semi: true,
-	bracketSpacing: true,
-	arrowParens: "always"
+  parser: "babylon",
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: "always",
+  singleQuote: false,
+  proseWrap: true
 };
